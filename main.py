03f0eb7aa3ddf60986e884cb87a3ce4aedbd45ee
XtraHigh <52545668+xtrahigh@users.noreply.github.com>
2021-01-12 04:00:06 +0700

    Update main.py
    - _id, _chat vars changed to _channel, because we will work with private channels
    - api_, _channel values changed to more understandable values
    - Removed unnecessary parameters in iter_messages
    - The first message in the shuffled music channel will now be pinned. This makes it easy to jump to the beginning of the playlist.
    - Added comments

--- main.py
+++ main.py
@@ -3,33 +3,47 @@ from telethon.tl.types import InputMessagesFilterMusic
 from random import shuffle
 
 # You must get your own api_id and api_hash from https://my.telegram.org
-api_id = 123
-api_hash = '123abc'
-from_id = -100190  # Tracks will be forwarded from this chat
-to_id = -100200  # Tracks will be forwarded to this chat
+api_id = 3333333
+api_hash = 'abc123'
+from_channel = -100333333333  # Tracks will be forwarded from this channel
+to_channel = -100444444444  # Tracks will be forwarded to this channel
 
 client = TelegramClient('tshuffler', api_id, api_hash)
 client.start()
 
 
 async def main():
+    # Creates a list with message IDs of your main music channel
     messages_ids = []
-    async for message in client.iter_messages(from_id, limit=None, filter=InputMessagesFilterMusic):
+    async for message in client.iter_messages(from_channel, None, filter=InputMessagesFilterMusic):
         messages_ids.append(message.id)
     print('Got', len(messages_ids), 'tracks to shuffle\n')
 
     shuffle(messages_ids)
 
+    # Creates a list with message IDs of your shuffled music channel
     to_messages_ids = []
-    async for to_message in client.iter_messages(to_id, limit=None, filter=InputMessagesFilterMusic):
+    async for to_message in client.iter_messages(to_channel, None, filter=InputMessagesFilterMusic):
         to_messages_ids.append(to_message.id)
 
-    await client.delete_messages(to_id, to_messages_ids)
-    print('Deleted', len(to_messages_ids), 'previous shuffled tracks\n')
+    # Clears your shuffled music channel from previous shuffled tracks
+    await client.delete_messages(to_channel, to_messages_ids)
+    print('Deleted', len(to_messages_ids), 'previous shuffled tracks\n\nShuffling tracks...\n')
 
+    # Forwards music from your main music channel to your shuffled music channel
+    # Method: message by message
     for messages_id in messages_ids:
-        await client.forward_messages(to_id, messages_id, from_id, silent=True)
-    print('Forwarded', len(messages_ids), 'shuffled tracks\n\nEnjoy your listening')
+        await client.forward_messages(to_channel, messages_id, from_channel, silent=True)
+    print('Forwarded', len(messages_ids), 'shuffled tracks\n')
+
+    # Creates a list with message IDs of your shuffled music channel
+    to_messages_ids = []
+    async for to_message in client.iter_messages(to_channel, None, filter=InputMessagesFilterMusic):
+        to_messages_ids.append(to_message.id)
+
+    # Pins the message with the first shuffled track to easily jump to it
+    await client.pin_message(to_channel, to_messages_ids[-1], notify=False)
+    print('First track pinned\n\nEnjoy your listening')
 
 with client:
     client.loop.run_until_complete(main())
